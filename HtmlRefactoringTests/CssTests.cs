1db42c2442861aa97466a2c9d282504739a9431a
ivo-tisljar <ivo.tisljar@gmail.com>
2023-03-20 10:31:18 +0100

    added properties Name And Value to class CssProperty and coresponding tests

--- HtmlRefactoringTests/CssTests.cs
+++ HtmlRefactoringTests/CssTests.cs
@@ -21,23 +21,30 @@ namespace HtmlRefactoringTests
             Throws<InvalidPropertyNameException>(() => new CssProperty(" :"));
             Throws<InvalidPropertyNameException>(() => new CssProperty("0:"));
             Throws<InvalidPropertyNameException>(() => new CssProperty("9a:0"));
+            Throws<InvalidPropertyNameException>(() => new CssProperty("x y :0"));
         }
 
         [Fact]
         public void WhenMissingPropertyValue_Throws()
         {
-            Throws<MissingPropertyValueException>(() => new CssProperty("x:"));
-            Throws<MissingPropertyValueException>(() => new CssProperty("x: "));
+            Throws<MissingPropertyValueException>(() => new CssProperty(" _x:"));
+            Throws<MissingPropertyValueException>(() => new CssProperty("x-1: "));
         }
 
-        //[Fact]
-        //public void AfterConstruction_CanReadPropertyName()
-        //{
-        //    var property = new CssProperty("x:");
+        [Fact]
+        public void AfterConstruction_CanReadPropertyName()
+        {
+            Equal("_x", new CssProperty("_x :0").Name);
+            Equal("x-y", new CssProperty(" x-y :0").Name);
+            Equal("x2", new CssProperty("x2:0").Name);
+        }
 
-        //Throws<InvalidPropertyNameException>(() => new CssProperty("w :"));
-
-        //Equals 
-        //}
+        [Fact]
+        public void AfterConstruction_CanReadPropertyValue()
+        {
+            Equal("0", new CssProperty("x:0").Value);
+            Equal("12 13", new CssProperty("x: 12 13 ").Value);
+            Equal("čžš", new CssProperty("x:    čžš").Value);
+        }
     }
 }
--- HtmlRefactoringWindowsApp/Css/CssProperty.cs
+++ HtmlRefactoringWindowsApp/Css/CssProperty.cs
@@ -11,43 +11,46 @@ namespace HtmlRefactoringWindowsApp.Css
     {
         private const char Colon = ':';
 
-        //public string PropertyName { get; set; }
+        private readonly string name;
+        public string Name { get { return name; } }
 
-        //public string PropertyValue { get; set; }
+        private readonly string value;
+        public string Value { get { return value; } }
 
         public CssProperty(string property) 
         {
             var colonIndex = FetchColonIndex(property);
-            VerifyPropertyName(property, colonIndex);
-            VerifyPropertyValue(property, colonIndex);
+            name = InitPropertyName(property, colonIndex);
+            value = InitPropertyValue(property, colonIndex);
         }
 
         private int FetchColonIndex(string property)
         {
             if (!property.Contains(Colon))
             {
-                throw new MissingColonException($"Invalid value! Property '{property}' does not contains colon '{Colon}'.");
+                throw new MissingColonException($"Error! Property '{property}' does not contains colon '{Colon}'.");
             }
             return property.IndexOf(Colon);
         }
 
-        private void VerifyPropertyName(string property, int colonIndex)
+        private string InitPropertyName(string property, int colonIndex)
         {
-            var reg = new Regex("^[a-zA-Z_-][0-9a-zA-Z_-]*\\s*$");
+            var reg = new Regex("^\\s*[a-zA-Z_-][0-9a-zA-Z_-]*\\s*$");
 
             if (!reg.IsMatch(property[..colonIndex]))
             {
-                throw new InvalidPropertyNameException($"Invalid value! Property '{property}' has invalid property-name.");
+                throw new InvalidPropertyNameException($"Error! Property '{property}' does not contain or has invalid property-name.");
             }
-            //return property[..colonIndex];
+            return property[..colonIndex].Trim();
         }
 
-        private void VerifyPropertyValue(string property, int colonIndex)
+        private string InitPropertyValue(string property, int colonIndex)
         {
             if (string.IsNullOrWhiteSpace(property[(colonIndex + 1)..]))
             {
-                throw new MissingPropertyValueException($"Invalid value! Property '{property}' does not contains property-value.");
+                throw new MissingPropertyValueException($"Error! Property '{property}' does not contains property-value.");
             }
+            return property[(colonIndex + 1)..].Trim();
         }
     }
     public class MissingColonException : Exception
