b0d4cc249f2a95f3122c8cd59fe3764c1b7e8023
ivo-tisljar <ivo.tisljar@gmail.com>
2023-04-09 01:14:36 +0200

    Added class CssTypes with CSS length validation function IsValidCssLength

---
+++ HtmlRefactoringWindowsApp/Css/CssTypes.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace HtmlRefactoringWindowsApp.Css
+{
+    public class CssTypes
+    {
+        //  px, em and % are the only three units used in CSS files exported from InDesign
+
+        public static bool IsValidCssLength(string cssLength)
+        {
+            var reg = new Regex("^(auto|-?\\d+(\\.\\d+)?(px|em|%)?)$");
+
+            return reg.IsMatch(cssLength);
+        }
+    }
+}
