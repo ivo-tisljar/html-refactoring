10033a1dd70afde5c94d481c606b003f1d6ace23
ivo-tisljar <ivo.tisljar@gmail.com>
2023-04-15 17:48:26 +0200

    Added new enum ReplacerMethod

---
+++ HtmlRefactoringWindowsApp/Enums/ReplacerMethod.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Xml.Linq;
+
+namespace HtmlRefactoringWindowsApp.Enums
+{
+    public enum ReplacerMethod
+    {
+        Text = 0,
+        Extended = 1,
+        Regex =2
+    }
+
+    public static class StringExtensionsForreplacerMethodString
+    {
+        public static ReplacerMethod ToReplacerMethod(this string replacerMethodString)
+        {
+            return (ReplacerMethod)Enum.Parse(typeof(ReplacerMethod), replacerMethodString);
+        }
+    }
+}
